2f8e7c069818e36a55e257221a363541968c9a0b
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-06-23 23:03:12 +0700

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,59 +1,51 @@
+import { css } from '@emotion/react';
+
 function Header({ coverImg, profileImg, displayName, description }: any) {
   return (
     <div className="flex justify-center">
-      <div className="w-full relative md:max-w-6xl flex-1">
+      <div className="relative w-full flex-1 md:max-w-6xl">
         <img
-          className="h-96 w-full object-cover rounded-b-lg"
+          className="h-96 w-full rounded-b-lg  bg-opacity-20 bg-gradient-to-br object-cover "
           src={coverImg}
-          alt=""
         />
-        <div className="-mt-48 flex items-center justify-center">
-          <div className="bg-white rounded-full p-3">
-            <img
-              className="rounded-full w-52 h-52 border object-cover"
-              src={profileImg}
-              alt=""
-            />
+        <div className=" -mt-48 flex items-center justify-center rounded  ">
+          <div className="rounded-full bg-white p-3">
+            <img className="h-52 w-52 rounded-full border object-cover" src={profileImg} alt="" />
           </div>
         </div>
         <div className=" flex flex-col space-y-4 border-b-2">
-          <h1 className="text-6xl text-semibold text-center text-gray-700">
-            {displayName}
-          </h1>
-          <h2 className="text-xl text-gray-500 text-center">{description}</h2>
+          <h1 className="text-semibold text-center text-6xl text-gray-700">{displayName}</h1>
+          <h2 className="text-center text-xl text-gray-500">{description}</h2>
         </div>
 
-        <div className="flex-wrap flex space-x-2 items-center justify-center">
-          <div className="p-5 rounded-lg hover:bg-gray-200 border-blue-500 items-center">
+        <div className="flex flex-wrap items-center justify-center space-x-2">
+          <div className="items-center rounded-lg border-blue-500 p-5 hover:bg-gray-200">
             <a href="https://github.com/npv2k1" target="_blank">
               <i className="fab fa-github fa-2x"></i>
             </a>
           </div>
-          <div className="p-5 rounded-lg hover:bg-gray-200 border-blue-500 items-center">
+          <div className="items-center rounded-lg border-blue-500 p-5 hover:bg-gray-200">
             <a href="https://www.linkedin.com/in/npv2k1/" target="_blank">
               <i className="fab fa-linkedin fa-2x text-[#0A66C2]"></i>
             </a>
           </div>
-          <div className="p-5 rounded-lg hover:bg-gray-200 border-blue-500 items-center">
+          <div className="items-center rounded-lg border-blue-500 p-5 hover:bg-gray-200">
             <a href="https://twitter.com/npv2k1" target="_blank">
               <i className="fab fa-twitter-square  fa-2x text-[#1DA1F2]"></i>
             </a>
           </div>
-          <div className="p-5 rounded-lg hover:bg-gray-200 border-blue-500 items-center">
+          <div className="items-center rounded-lg border-blue-500 p-5 hover:bg-gray-200">
             <a href="https://www.facebook.com/pvnpvnpvn/" target="_blank">
               <i className="fab fa-facebook-square fa-2x text-[#0F90F2]"></i>
             </a>
           </div>
-          <div className="p-5 rounded-lg hover:bg-gray-200 border-blue-500 items-center">
+          <div className="items-center rounded-lg border-blue-500 p-5 hover:bg-gray-200">
             <a href="https://2k1.org" target="_blank">
               <i className="fab fa-wordpress fa-2x text-[#464646]"></i>
             </a>
           </div>
-          <div className="p-5 rounded-lg hover:bg-gray-200 border-blue-500 items-center">
-            <a
-              href="https://www.youtube.com/channel/UCXsIcA5FfZIOZUOpFRrTcHA"
-              target="_blank"
-            >
+          <div className="items-center rounded-lg border-blue-500 p-5 hover:bg-gray-200">
+            <a href="https://www.youtube.com/channel/UCXsIcA5FfZIOZUOpFRrTcHA" target="_blank">
               <i className="fab fa-youtube fa-2x text-[#FF0000]"></i>
             </a>
           </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import Header from "@/components/Header";
-import Posts from "@/components/Posts";
-import Sidebar from "@/components/Sidebar";
+import Header from '@/components/Header';
+import Posts from '@/components/Posts';
+import Sidebar from '@/components/Sidebar';
 export default function Home() {
-  const displayName = "Nguyen Pham";
-  const description = "@npv2k1";
-  const coverImg = "cover.webp";
-  const profileImg ="meface.jpg";
+  const displayName = 'Nguyen Pham';
+  const description = '@npv2k1';
+  const coverImg = 'img/background.png';
+  const profileImg = 'img/avatar.jpg';
   return (
     <div className="flex flex-col">
       <Header
         displayName={displayName}
         coverImg={coverImg}
         profileImg={profileImg}
-        description={description}        
+        description={description}
       />
-      <div className="pb-96 h-full bg-[#F0F2F5]">
-        <div className="max-w-6xl mx-auto flex flex-col md:flex-row  justify-center space-x-2">
+      <div className="h-full bg-[#F0F2F5] pb-96">
+        <div className="mx-auto flex max-w-6xl flex-col justify-center  space-x-2 md:flex-row">
           <Sidebar />
           <Posts />
         </div>
