ff6313bfd2d00c3a5e8c3d26f79c87ff22a93185
npv2k1 <73846954+npv2k1@users.noreply.github.com>
2024-05-05 09:50:25 +0000

    Deploying to gh-pages from @ npv2k1/npv2k1.github.io@ad795a7cfe48acf55c7e6faf42c3e2bd272dbcf8 🚀

--- _next/static/Jmy837k6Il3MToO4JMka6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/463-2be752d330ab8388.js","static/chunks/pages/index-3f04814b742d8b5e.js"],"/_error":["static/chunks/pages/_error-1d28e89ede5693a1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/pages/_error-9e7644ba2b7ca8e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{323:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(5673)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=323)}),_N_E=n.O()}]);
